dcc8bee621a769c3934132a7a507cd10c2f9cb14
Shynggys Shynbolatov <shyngys.shynbolatov@gmail.com>
2024-01-13 23:54:57 +0700

    files uploaded

---
+++ daily_db_backup.ts
@@ -0,0 +1,41 @@
+import Logger from "https://deno.land/x/logger@v1.1.2/logger.ts";
+const logger = new Logger();
+
+async function run_db_backup() {
+    // Get current date and time in a specific format (e.g., YYYY-MM-DD_HH-MM-SS)
+    const now = new Date();
+    const timestamp = now.toISOString().replace(/T/, '_').replace(/\..+/, '').replace(/:/g, '-');
+
+    const backupFileName = `./db_backups/dump_${timestamp}.sql`;
+
+    const process = Deno.run({
+    cmd: [
+        "pg_dump",
+        "-U", "postgres", // Database username
+        "-d", "postgres", // Database name
+        "-f", backupFileName // Path to save the backup file with timestamp
+    ],
+    stdout: "piped",
+    stderr: "piped",
+    });
+
+    const { code } = await process.status();
+
+    if (code === 0) {
+      const output = await process.output();
+      logger.info(new TextDecoder().decode(output));
+    } else {
+      const error = await process.stderrOutput();
+      logger.error(new TextDecoder().decode(error));
+    }
+}
+
+async function main() {
+    while (true) {
+      await run_db_backup();
+      // Wait for 24 hours (24 hours * 60 minutes * 60 seconds * 1000 milliseconds)
+      await new Promise(resolve => setTimeout(resolve, 24 * 60 * 60 * 1000));
+    }
+  }
+  
+  main();
